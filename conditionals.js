ad3739f2fb2a83c92192e176446c6879ec382d7b
Julia Ballo <ballo.julia@gmail.com>
2020-10-25 15:48:06 -0700

    First Commit

---
+++ conditionals.js
@@ -0,0 +1,44 @@
+//exercise 21
+function isEven(num){
+   if(num % 2 === 0) {
+       console.log("even");
+   } 
+}
+
+isEven(4);
+isEven(3);
+   
+//exercise 22
+function getColor(phrase){
+    if(phrase === "stop") {
+        console.log("red");
+    } else if(phrase === "slow") {
+        console.log(yellow);
+    } else if(phrase === "go") {
+        console.log("green");
+    } else {
+        console.log("purple");
+    }
+}
+
+getColor("stop");
+getColor("slow");
+getColor("go");
+getColor("what?");
+
+//exercise 23
+// Change the value of num, so that "YOU GOT ME!" prints out
+const num = 102; // THIS IS THE ONLY LINE YOU SHOULD CHANGE 
+
+// DO NOT TOUCH ANYTHING BELOW (please) 
+if(num <= 100) {
+    if(num >= 50) {
+        console.log("HEY!");
+    }
+} else {
+    if (num < 103) {
+        if(num % 2 === 0){
+            console.log("YOU GOT ME!");
+        }
+    }
+}
